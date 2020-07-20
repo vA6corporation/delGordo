e89900b1f7cb21dbf8be60281305095f9721cd8d
agrimald2 <45186396+agrimald2@users.noreply.github.com>
2020-07-20 01:50:43 -0500

    Vistas del landing + Carrito Diseño

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,36 @@
+<section id="inicio" class="centrar">
+    <div id="carouselHome" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators home_CI">
+            <li data-target="#carouselHome" data-slide-to="0" class="active home_CII"></li>
+            <li data-target="#carouselHome" data-slide-to="1" class="home_CII"></li>
+            <li data-target="#carouselHome" data-slide-to="2" class="home_CII"></li>
+        </ol>
+        <div class="carousel-inner" id="ca_mobile">
+            <div class="carousel-item active carrou1">
+            </div>
+            <div class="carousel-item carrou2">
+            </div>
+            <div class="carousel-item carrou3">
+            </div>
+        </div>
+
+        <a class="carousel-control-prev" href="#carouselHome" role="button" data-slide="prev" style="width: 15vw;">
+            <span aria-hidden="true"><i class="fas fa-long-arrow-alt-left home_arrows_carrousel"></i></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselHome" role="button" data-slide="next" style="width: 15vw;">
+            <span aria-hidden="true"><i class="fas fa-long-arrow-alt-right home_arrows_carrousel"></i></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <a href="#store" type="button" onclick="scroll()" class="btn" id="shop_now">COMPRA AQUÍ</a>
+</section>
+<script type="text/javascript">
+/*(function () { var ldk = document.createElement('script'); 
+               ldk.type = 'text/javascript'; 
+               ldk.async = true; 
+               ldk.src = 'https://s.cliengo.com/weboptimizer/5f0f9cf43b6953002a3d4876/5f0f9d63e2f723002a86f995.js';
+               var s = document.getElementsByTagName('script')[0]; 
+               s.parentNode.insertBefore(ldk, s); })();
+*/
+</script>
---
+++ resources/views/us.blade.php
@@ -0,0 +1,3 @@
+<section id="us" style="width:100%">
+    <img src="./assets/images/mobile/us.webp" style="width: 100%;">
+</section>
